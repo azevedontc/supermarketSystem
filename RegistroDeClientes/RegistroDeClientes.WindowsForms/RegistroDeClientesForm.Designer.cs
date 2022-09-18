de31a69c17a38e5ac59f9dd60aa15fdd4db7af09
Augusto Azevedo <91835169+azevedontc@users.noreply.github.com>
2022-09-17 23:26:03 -0300

    Add files via upload

---
+++ RegistroDeClientes/RegistroDeClientes.WindowsForms/RegistroDeClientesForm.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace RegistroDeClientes.WindowsForms
+{
+    partial class RegistroDeClientesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegistroDeClientesForm));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Logo = new System.Windows.Forms.PictureBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Cadastro = new System.Windows.Forms.Label();
+            this.EMail = new System.Windows.Forms.Label();
+            this.Tel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Nome = new System.Windows.Forms.Label();
+            this.Save = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.Logo, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox3, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 7);
+            this.tableLayoutPanel1.Controls.Add(this.textBox4, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 0, 9);
+            this.tableLayoutPanel1.Controls.Add(this.Cadastro, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.EMail, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.Tel, 0, 6);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 8);
+            this.tableLayoutPanel1.Controls.Add(this.Nome, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.Save, 0, 10);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 11;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 35F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(704, 473);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // Logo
+            // 
+            this.Logo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.Logo.Image = ((System.Drawing.Image)(resources.GetObject("Logo.Image")));
+            this.Logo.Location = new System.Drawing.Point(3, 3);
+            this.Logo.Name = "Logo";
+            this.Logo.Size = new System.Drawing.Size(698, 149);
+            this.Logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.Logo.TabIndex = 3;
+            this.Logo.TabStop = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Location = new System.Drawing.Point(3, 209);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(698, 23);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(3, 341);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(698, 23);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox4.Location = new System.Drawing.Point(3, 275);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(698, 23);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(3, 407);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(698, 23);
+            this.textBox2.TabIndex = 7;
+            // 
+            // Cadastro
+            // 
+            this.Cadastro.AutoSize = true;
+            this.Cadastro.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Cadastro.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Cadastro.Location = new System.Drawing.Point(3, 155);
+            this.Cadastro.Name = "Cadastro";
+            this.Cadastro.Size = new System.Drawing.Size(698, 22);
+            this.Cadastro.TabIndex = 8;
+            this.Cadastro.Text = "Faça seu cadastro:";
+            this.Cadastro.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // EMail
+            // 
+            this.EMail.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.EMail.AutoSize = true;
+            this.EMail.Location = new System.Drawing.Point(3, 246);
+            this.EMail.Name = "EMail";
+            this.EMail.Size = new System.Drawing.Size(44, 15);
+            this.EMail.TabIndex = 9;
+            this.EMail.Text = "E-Mail:";
+            // 
+            // Tel
+            // 
+            this.Tel.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.Tel.AutoSize = true;
+            this.Tel.Location = new System.Drawing.Point(3, 312);
+            this.Tel.Name = "Tel";
+            this.Tel.Size = new System.Drawing.Size(54, 15);
+            this.Tel.TabIndex = 10;
+            this.Tel.Text = "Telefone:";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 378);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 15);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Senha:";
+            // 
+            // Nome
+            // 
+            this.Nome.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.Nome.AutoSize = true;
+            this.Nome.Location = new System.Drawing.Point(3, 180);
+            this.Nome.Name = "Nome";
+            this.Nome.Size = new System.Drawing.Size(99, 15);
+            this.Nome.TabIndex = 12;
+            this.Nome.Text = "Nome Completo:";
+            this.Nome.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Save
+            // 
+            this.Save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.Save.Location = new System.Drawing.Point(314, 444);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 26);
+            this.Save.TabIndex = 13;
+            this.Save.Text = "Salvar";
+            this.Save.UseVisualStyleBackColor = true;
+            // 
+            // RegistroDeClientesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 473);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximizeBox = false;
+            this.Name = "RegistroDeClientesForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Registro de Clientes";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private PictureBox Logo;
+        private TextBox textBox3;
+        private TextBox textBox1;
+        private TextBox textBox4;
+        private TextBox textBox2;
+        private Label Cadastro;
+        private Label EMail;
+        private Label Tel;
+        private Label label4;
+        private Label Nome;
+        private Button Save;
+    }
+}
